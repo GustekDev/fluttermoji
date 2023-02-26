42ef0fe0ab075b3532130ef0d0eac1cd6532f285
GustekDev <panguzol@gmail.com>
2023-02-26 13:21:13 +0000

    semantic label

--- lib/fluttermojiCircleAvatar.dart
+++ lib/fluttermojiCircleAvatar.dart
@@ -13,16 +13,17 @@ class FluttermojiCircleAvatar extends StatelessWidget {
   final double radius;
   final Color backgroundColor;
   final Fluttermoji emoji;
+  final String label;
 
   FluttermojiCircleAvatar(this.emoji,
-      {Key? key, this.radius = 75.0, required this.backgroundColor})
+      {Key? key, this.radius = 75.0, required this.backgroundColor, this.label = "Fluttermoji"})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var svg = SvgPicture.string(
       emoji.getSVG(),
-      semanticsLabel: "Your Fluttermoji",
+      semanticsLabel: label,
       placeholderBuilder: (context) => Center(
         child: CupertinoActivityIndicator(),
       ),
